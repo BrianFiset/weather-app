1022334ae5939f58859f4b4cc106c008183c2587
Brian <fisetbrian@gmail.com>
2023-11-02 15:20:19 -0400

    Add webpack

--- app.js
+++ app.js
@@ -1,19 +1,9 @@
-let weatherData;
 async function getWeather(location) {
     try {
         const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=31b5dcbb872d413bbd5143944230510&q=${location}&aqi=no`);
-        weatherData = await response.json();
-        console.log(weatherData.current.condition.text)
+        const weatherData = await response.json();
+        console.log(weatherData)
     } catch(err) {
         console.log(err)
     }
 }
-
-getWeather('Costa rica');
-
-// function getWeatherData(url) {
-//     let p = Request(url)
-//     return new Promise((resolve,reject) {
-
-//     })
-// }
